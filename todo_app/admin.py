3b2c5217bf19e9a0188ecce17c5c56f291b7273a
gundyn <nathan.gundy@gmail.com>
2023-05-02 21:24:43 -0500

    feat: Add sample to-do data using Django admin interface
    - Registered ToDoList and ToDoItem models in admin.py
    - Created a superuser to access the admin site
    - Added one list and one item using the admin interface

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,6 @@
+# todo_list/todo_app/admin.py
 from django.contrib import admin
+from .models import ToDoList, ToDoItem
 
-# Register your models here.
+admin.site.register(ToDoList)
+admin.site.register(ToDoItem)
