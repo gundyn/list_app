3152149f54c9e3b9b1a8c0f0f78ea7ef5d02f4ca
gundyn <nathan.gundy@gmail.com>
2023-05-09 19:28:48 -0500

    feat: add unit tests for models
    This commit introduces unit tests for the ToDoList and ToDoItem models.
    The tests verify the correct creation of both to-do lists and to-do items.
    This forms the basis for further testing as the application's features evolve.

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -1,3 +1,20 @@
+# todo_list/todo_app/test.py
 from django.test import TestCase
+from .models import ToDoList, ToDoItem
 
-# Create your tests here.
+class ToDoListTestCase(TestCase):
+    def test_todo_list_creation(self):
+        list_title = "Test List"
+        todo_list = ToDoList.objects.create(title=list_title)
+        self.assertEqual(todo_list.title, list_title)
+        
+class TodoItemTestCase(TestCase):
+    def test_todo_item_creation(self):
+        list_title = "Test List"
+        todo_list = ToDoList.objects.create(title=list_title)
+
+        item_title = "Test Item"
+        todo_item = ToDoItem.objects.create(title=item_title, todo_list=todo_list)
+        self.assertEqual(todo_item.title, item_title)
+        self.assertEqual(todo_item.todo_list, todo_list)
+
